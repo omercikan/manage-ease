d6661e9ee0ef5b4648c958c7319fa8691f5e66d9
omercikan <omercikan36@gmail.com>
2025-01-01 17:31:09 +0300

    Some packages created and Updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js,jsx,tsx,ts}"],
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
